ae95b381b29b62c7ef4c676592d75425fd5dc71d
afifahratma <afifahratma@gmail.com>
2021-05-02 11:05:16 +0700

    added winston log to handle error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const winston = require('winston')
 const app = express();
 require('dotenv').config();
 const coworkRoute = require('./routes/coworking')
@@ -10,21 +11,38 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+// create a logger
+ 
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format:winston.format.combine(
+        winston.format.colorize({all:true})
+      )
+    }),
+    new winston.transports.File({ filename: 'error.log', level:'error'})
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'exceptions.log' })
+  ]
+});
+
 //routes
 app.use('/api/coworks', coworkRoute);
 
+
 //connect to mongodb atlas
 mongoose.connect(process.env.MONGO_URL,
-    {useNewUrlParser: true, useUnifiedTopology: true})
+    {useNewUrlParser: true, useUnifiedTopology: true}) //beda
   .then(() => {
-    console.log("Connected to MongoDB Atlas");
+    logger.info("Connected to MongoDB Atlas");
   }).catch(error => {
-      console.log("Something Wrong happend", error);
+      logger.error("error", error.message);
   })
 
 
 
 //start server
 app.listen(PORT, () => {
-  console.log("Server started at PORT ", PORT);
+  logger.info(`Server started at PORT ${PORT}`);
 });
--- routes/coworking.js
+++ routes/coworking.js
@@ -1,14 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Cowork, validateCowork} = require('../models/cowork')
+const { Cowork, validateCowork } = require("../models/cowork");
 
 // POST : CREATE A NEW COWORKING SPACE DATA
-router.post('/', async (req,res) => {
-    const error = await validateCowork(req.body);
+router.post("/", async (req, res) => {
+  const error = await validateCowork(req.body);
 
-    if(error.message) res.status(400).send(error.message);
+  if (error.message) res.status(400).send(error.message);
 
-    cowork = new Cowork({
+  cowork = new Cowork({
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.price,
+    address: req.body.address,
+    district: req.body.district,
+    city: req.body.city,
+    fasilitas: req.body.fasilitas,
+    open: req.body.open,
+    close: req.body.close,
+    phone: req.body.phone,
+    region: {
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+    },
+  });
+
+  cowork
+    .save()
+    .then((cowork) => {
+      res.send(cowork);
+    })
+    .catch((error) => {
+      res.status(500).send("Data was not stored in db");
+    });
+});
+
+//GET ALL DATA COWORKING
+router.get("/", (req, res) => {
+  Cowork.find()
+    .then((coworks) => res.send(coworks))
+    .catch((error) => {
+      res.status(500).send("Something went Wrong!");
+    });
+});
+
+//GET DATA COWORKING BY ID
+router.get("/:coworkId", async (req, res) => {
+  const cowork = await Cowork.findById(req.params.coworkId);
+  if(!cowork) res.status(404).send("Coworking Space not found")
+  res.send(cowork)
+});
+
+//UPDATE BY ID
+router.put("/:coworkId", async (req,res) => {
+    const updateCowork = await Cowork.findByIdAndUpdate(req.params.coworkId, {
         name: req.body.name,
         image: req.body.image,
         price: req.body.price,
@@ -21,15 +66,18 @@ router.post('/', async (req,res) => {
         phone: req.body.phone,
         region: {
             latitude: req.body.latitude,
-            longitude: req.body.longitude
-        }
-    })
+            longitude: req.body.longitude,
+        },
+    },{new: true}) 
 
-    cowork.save().then((cowork) => {
-        res.send(cowork);
-    }).catch(error => {
-        res.status(500).send("Data was not stored in db")
-    })
+    if(!updateCowork) res.status(404).send("Coworking Space not found")
+    res.send(updateCowork)
 })
 
+//DELETE BASED ON ID
+router.delete('/:coworkId', async (req,res) => {
+    const cowork = await Cowork.findByIdAndRemove(req.params.coworkId)
+    if(!cowork) res.status(404).send('coworking with id not found')
+    res.send(cowork)
+})
 module.exports = router;
